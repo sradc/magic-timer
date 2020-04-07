2f238c38ae7f431e4fe9820ffa581d6d1e31c29e
sradc <sradc@users.noreply.github.com>
2020-04-07 11:30:01 +0100

    added missing asserts

--- tests/test_format_output.py
+++ tests/test_format_output.py
@@ -2,11 +2,11 @@ from magic_timer import format_output
 
 def test_format_output():
     assert format_output(365*24*60*60) == "370 days"
-    format_output(2*24*60*60) == "2.0 days"
-    format_output(4.2*60*60) == "4.2 hours"
-    format_output(3.11*60) == "3.2 minutes"
-    format_output(45.38) == "46 seconds"
-    format_output(.334) == "340 milliseconds"
-    format_output(.00008422) == "85 microseconds"
-    format_output(.0) == "t < 0.05 microseconds"
+    assert format_output(2*24*60*60) == "2.0 days"
+    assert format_output(4.2*60*60) == "4.2 hours"
+    assert format_output(3.11*60) == "3.2 minutes"
+    assert format_output(45.38) == "46 seconds"
+    assert format_output(.334) == "340 milliseconds"
+    assert format_output(.00008422) == "85 microseconds"
+    assert format_output(.0) == "t < 0.05 microseconds"
     
