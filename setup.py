4ed3f1d08c36a40f2441dd9191563ccf59610f10
sradc <sidneyradcliffe@sky.com>
2022-04-02 17:12:49 +0100

    chore: increment version... todo: add automatic semantic versioning...

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="magic-timer",
-    version="0.1.1",
+    version="0.2.0",
     author="Sidney Radcliffe",
     author_email="sidneyradcliffe@gmail.com",
     description="Conveniently get a rough idea of how long things take.",
